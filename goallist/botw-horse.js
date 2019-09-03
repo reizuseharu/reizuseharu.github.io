3bd0cb2ffa6e59d82ec62fab13905644732c81e9
reizuseharu <32425788+reizuseharu@users.noreply.github.com>
2019-09-02 21:09:30 -0400

    Rearranged entries by difficulty

--- goallist/botw-horse.js
+++ goallist/botw-horse.js
@@ -1,106 +1,106 @@
 var bingoList = [];
   bingoList[1] = [
-    { name: "Register Ganonhorse [Giant Horse] without stamina food", types: ["GANON", "HORSE", "STAMINA"]},
-    { name: "Register Zelda's Hereditary Horse [White Horse] without stamina food", types: ["ZELDA", "HORSE", "STAMINA"]},
-    { name: "Free all horses from a gang of Bokoblins", types: ["BATTLE", "HORSE"]}
+    { name: "Tame a horse after landing on it through gliding", types: ["AFFECTION", "HORSE"]},
+    { name: "Tame a horse after sneaking up on it", types: ["AFFECTION", "HORSE"]},
   ];
   bingoList[2] = [
     { name: "Take 3 pictures of different groups of horses", types: ["PICTURE", "HORSE"] },
-    { name: "Register Zelda's Hereditary Horse [White Horse] without stamina upgrades", types: ["ZELDA", "REGISTER", "HORSE", "STAMINA"]},
-    { name: "Max a horse's affection", types: ["AFFECTION", "HORSE"]}
+    { name: "Register a solid color horse", types: ["HORSE", "COLOR", "REGISTER"]},
   ];
   bingoList[3] = [
-    { name: "Register Ganonhorse [Giant Horse] without stamina upgrades", types: ["GANON", "REGISTER", "HORSE", "STAMINA"]},
-    { name: "Ride a stalhorse to Kilton's Fang and Bone", types: ["STALHORSE", "TRAVEL"]},
-    { name: "Max 5 horses' affection", types: ["AFFECTION", "HORSE"]}
+    { name: "Take 5 pictures of boars running at you", types: ["BOAR", "PICTURE", "BATTLE"]},
+    { name: "Steal a horse from a Bokoblin", types: ["BATTLE", "HORSE"]},
   ];
   bingoList[4] = [
-    { name: "Ride a bear to a stable", types: ["BEAR", "TRAVEL"]},
-    { name: "Ride a buck to a stable", types: ["MOOSE", "TRAVEL"]},
-    { name: "Tame a horse after landing on it through gliding", types: ["AFFECTION", "HORSE"]}
+    { name: "Ride a Lynel", types: ["LYNEL", "BATTLE"]},
   ];
   bingoList[5] = [
-    { name: "Ride a doe to a stable", types: ["MOOSE", "TRAVEL"]},
-    { name: "Ride Satori [Lord of the Mountain] to a stable", types: ["SATORI", "TRAVEL"]},
-    { name: "Tame a horse after sneaking up on it", types: ["AFFECTION", "HORSE"]}
+    { name: "Ride a horse to Gerudo Desert", types: ["HORSE", "VILLAGE", "TRAVEL"]},
+    { name: "Ride a horse to Rito Village", types: ["HORSE", "VILLAGE", "TRAVEL"]},
+    { name: "Ride a horse to Hateno Village", types: ["HORSE", "VILLAGE", "TRAVEL"]},
+    { name: "Ride a horse to Kakariko Village", types: ["HORSE", "VILLAGE", "TRAVEL"]},
+    { name: "Ride a horse to Lurelin Village", types: ["HORSE", "VILLAGE", "TRAVEL"]},
   ];
   bingoList[6] = [
-    { name: "Register 5 horses", types: ["REGISTER", "HORSE"]},
-    { name: "Unleash the horse god", types: ["HORSE_GOD"]}
+    { name: "Get a Lynel's attention and escape on a horse", types: ["LYNEL", "HORSE"]},
   ];
   bingoList[7] = [
-    { name: "Ride a Lynel", types: ["LYNEL", "BATTLE"]},
-    { name: "Ride a boar to a stable", types: ["BOAR", "TRAVEL"]}
+    { name: "Register a horse at one stable and ride to another", types: ["HORSE", "TRAVEL", "REGISTER"]},
   ];
   bingoList[8] = [
-    { name: "Take 5 pictures of boars running at you", types: ["BOAR", "PICTURE", "BATTLE"]},
-    { name: "Ride a horse to Zora's Domain", types: ["HORSE", "VILLAGE", "TRAVEL"]}
+    { name: "Free all horses from a gang of Bokoblins", types: ["BATTLE", "HORSE"]},
   ];
   bingoList[9] = [
-    { name: "Ride a horse to Death Mountain Marker #2", types: ["HORSE", "VILLAGE", "TRAVEL"]},
-    { name: "Ride a horse to Gerudo Village", types: ["HORSE", "VILLAGE", "TRAVEL"]}
+    { name: "Max a horse's affection", types: ["AFFECTION", "HORSE"]},
+    { name: "Register 5 horses", types: ["REGISTER", "HORSE"]},
   ];
   bingoList[10] = [
-    { name: "Ride a horse to Rito Village", types: ["HORSE", "VILLAGE", "TRAVEL"]},
-    { name: "Ride a horse to Hateno Village", types: ["HORSE", "VILLAGE", "TRAVEL"]}
+    { name: "Take pictures of 3 possible horse coat colors [brown/light brown/black/gray/blue/white]", types: ["COLOR", "PICTURE", "HORSE"]},
+    { name: "Take pictures of 3 possible horse mane colors [brown/light brown/black/gray/blue/white]", types: ["COLOR", "PICTURE", "HORSE"]},
+    { name: "Take pictures of 3 possible horse snout colors [brown/light brown/black/gray/blue/white]", types: ["COLOR", "PICTURE", "HORSE"]},
+    { name: "Take pictures of 2 possible horse ankle colors/lengths [short/long white/black]", types: ["COLOR", "PICTURE", "HORSE"]},
   ];
   bingoList[11] = [
-    { name: "Ride a horse to Kakariko Village", types: ["HORSE", "VILLAGE", "TRAVEL"]},
-    { name: "Ride a horse to Lurelin Village", types: ["HORSE", "VILLAGE", "TRAVEL"]}
+    { name: "Ride a horse from Gerudo to Akkala or vice versa", types: ["TRAVEL", "HORSE"]},
+    { name: "Ride a horse from Faron to Hebra or vice versa", types: ["TRAVEL", "HORSE"]},
   ];
   bingoList[12] = [
-    { name: "Ride a horse to Hyrule Castle", types: ["HORSE", "TRAVEL"]},
-    { name: "Ride a sandseal to each molduga in Gerudo Desert", types: ["SANDSEAL", "TRAVEL"]}
+    { name: "Take a picture of both types of boars [Woodland / Red-Tusked]", types: ["BOAR", "PICTURE"]},
+    { name: "Take a picture of both types of bears [Honeyvore / Grizzlemaw]", types: ["BEAR", "PICTURE"]},
   ];
   bingoList[13] = [
-    { name: "Take a picture of every rideable mount", types: ["PICTURE", "SANDSEAL", "STALHORSE", "BOAR", "BEAR", "ZELDA", "GANON", "HORSE"] },
-    { name: "Take a picture of both types of boars [Woodland / Red-Tusked]", types: ["BOAR", "PICTURE"]}
+    { name: "Register Ganonhorse [Giant Horse] without stamina food", types: ["GANON", "HORSE", "STAMINA"]},
+    { name: "Register Zelda's Hereditary Horse [White Horse] without stamina food", types: ["ZELDA", "HORSE", "STAMINA"]},
   ];
   bingoList[14] = [
-    { name: "Take a picture of both types of bears [Honeyvore / Grizzlemaw]", types: ["BEAR", "PICTURE"]},
-    { name: "Register a solid color horse", types: ["HORSE", "COLOR", "REGISTER"]}
+    { name: "Register Zelda's Hereditary Horse [White Horse] without stamina upgrades", types: ["ZELDA", "REGISTER", "HORSE", "STAMINA"]},
+    { name: "Register Ganonhorse [Giant Horse] without stamina upgrades", types: ["GANON", "REGISTER", "HORSE", "STAMINA"]},
   ];
   bingoList[15] = [
-    { name: "Take pictures of all possible horse coat colors [brown/light brown/black/gray/blue/white]", types: ["HORSE", "PICTURE", "COLOR"]},
-    { name: "Take pictures of all possible horse mane colors [brown/light brown/black/gray/blue/white]", types: ["HORSE", "PICTURE", "COLOR"]}
+    { name: "Max 5 horses' affection", types: ["AFFECTION", "REGISTER", "HORSE"]},
   ];
   bingoList[16] = [
-    { name: "Take pictures of all possible horse snout colors [brown/light brown/black/gray/blue/white]", types: ["HORSE", "PICTURE", "COLOR"]},
-    { name: "Take pictures of all possible horse ankle colors/lengths [short/long white/black]", types: ["HORSE", "PICTURE", "COLOR"]}
+    { name: "Ride a stalhorse to Kilton's Fang and Bone", types: ["STALHORSE", "TRAVEL"]},
+    { name: "Ride a bear to a stable", types: ["BEAR", "TRAVEL"]},
+    { name: "Ride a buck to a stable", types: ["MOOSE", "TRAVEL"]},
+    { name: "Ride a doe to a stable", types: ["MOOSE", "TRAVEL"]},
+    { name: "Ride a boar to a stable", types: ["BOAR", "TRAVEL"]},
   ];
   bingoList[17] = [
-    { name: "Take a picture of a horse on top of a Talus", types: ["HORSE", "PICTURE"]},
-    { name: "Ride a stalhorse to the Satori mountain meeting spot", types: ["STALHORSE", "TRAVEL"]}
+    { name: "Ride Satori [Lord of the Mountain] to a stable", types: ["SATORI", "TRAVEL"]},
   ];
   bingoList[18] = [
-    { name: "Ride a horse to the shrine of Courage, Wisdom, or Power", types: ["HORSE", "TRAVEL"]},
-    { name: "Register a horse at one stable and ride to another", types: ["HORSE", "TRAVEL", "REGISTER"]}
+    { name: "Ride a horse to Death Mountain Marker #2", types: ["HORSE", "VILLAGE", "TRAVEL"]},
+    { name: "Ride a horse to Zora's Domain", types: ["HORSE", "VILLAGE", "TRAVEL"]},
   ];
   bingoList[19] = [
-    { name: "Get a Lynel's attention and escape on a horse", types: ["LYNEL", "HORSE"]},
-    { name: "Defeat a Hinox on horseback", types: ["BATTLE", "HORSE"]}
+    { name: "Ride a horse to Hyrule Castle", types: ["HORSE", "TRAVEL"]},
+    { name: "Ride the same horse, without dismounting, to 3 stables", types: ["TRAVEL", "HORSE"]},
   ];
   bingoList[20] = [
-    { name: "Defeat a Lynel on horseback", types: ["BATTLE", "HORSE"]},
-    { name: "Defeat a Talus on horseback", types: ["BATTLE", "HORSE"]}
+    { name: "Take a picture of a horse on top of a Talus", types: ["HORSE", "PICTURE"]},
+    { name: "Ride a stalhorse to the Satori mountain meeting spot", types: ["STALHORSE", "TRAVEL"]},
   ];
   bingoList[21] = [
-    { name: "Defeat a Guardian on horseback", types: ["BATTLE", "HORSE"]},
-    { name: "Defeat a Molduga while being pulled by a sandseal", types: ["BATTLE", "SANDSEAL"]}
+    { name: "Ride a horse to the shrine of Courage, Wisdom, or Power", types: ["HORSE", "TRAVEL"]},
+    { name: "Ride a sandseal to each molduga in Gerudo Desert", types: ["SANDSEAL", "TRAVEL"]},
   ];
   bingoList[22] = [
-    { name: "Take pictures of 3 possible horse coat colors [brown/light brown/black/gray/blue/white]", types: ["COLOR", "PICTURE", "HORSE"]},
-    { name: "Take pictures of 3 possible horse mane colors [brown/light brown/black/gray/blue/white]", types: ["COLOR", "PICTURE", "HORSE"]}
+    { name: "Take a picture of every rideable mount", types: ["PICTURE"] },
   ];
   bingoList[23] = [
-    { name: "Take pictures of 3 possible horse snout colors [brown/light brown/black/gray/blue/white]", types: ["COLOR", "PICTURE", "HORSE"]},
-    { name: "Take pictures of 2 possible horse ankle colors/lengths [short/long white/black]", types: ["COLOR", "PICTURE", "HORSE"]}
+    { name: "Defeat a Hinox on horseback", types: ["BATTLE", "HORSE"]},
+    { name: "Defeat a Talus on horseback", types: ["BATTLE", "HORSE"]},
+    { name: "Defeat a Guardian on horseback", types: ["BATTLE", "HORSE"]},
+    { name: "Defeat a Lynel on horseback", types: ["BATTLE", "HORSE"]},
+    { name: "Defeat a Molduga while being pulled by a sandseal", types: ["BATTLE", "SANDSEAL"]},
   ];
   bingoList[24] = [
-    { name: "Ride the same horse, without dismounting, to 3 stables", types: ["TRAVEL", "HORSE"]},
-    { name: "Steal a horse from a Bokoblin", types: ["BATTLE", "HORSE"]}
+    { name: "Take pictures of all possible horse coat colors [brown/light brown/black/gray/blue/white]", types: ["HORSE", "PICTURE", "COLOR"]},
+    { name: "Take pictures of all possible horse mane colors [brown/light brown/black/gray/blue/white]", types: ["HORSE", "PICTURE", "COLOR"]},
+    { name: "Take pictures of all possible horse snout colors [brown/light brown/black/gray/blue/white]", types: ["HORSE", "PICTURE", "COLOR"]},
+    { name: "Take pictures of all possible horse ankle colors/lengths [short/long white/black]", types: ["HORSE", "PICTURE", "COLOR"]},
   ];
   bingoList[25] = [
-    { name: "Ride a horse from Gerudo to Akkala or vice versa", types: ["TRAVEL", "HORSE"]},
-    { name: "Ride a horse from Faron to Hebra or vice versa", types: ["TRAVEL", "HORSE"]}
+    { name: "Unleash the horse god", types: ["HORSE_GOD"]},
   ];
