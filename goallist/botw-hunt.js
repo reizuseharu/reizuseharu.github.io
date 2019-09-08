389b1e39ce9c40f312a7290bd0caef56e86420ff
reizuseharu <32425788+reizuseharu@users.noreply.github.com>
2019-09-08 03:13:42 -0400

    BOTW Hunt Bingo goal

---
+++ goallist/botw-hunt.js
@@ -0,0 +1,114 @@
+var bingoList = [];
+  bingoList[1] = [
+    { name: "Kill a Fox", types: ["CREATURE"]},
+  ];
+  bingoList[2] = [
+    { name: "Kill any 3 birds", types: ["CREATURE"]},
+    { name: "Kill a Bushy-Tailed Squirrel", types: ["CREATURE"]},
+    { name: "Shoot a Bluepee", types: ["CREATURE"]},
+  ];
+  bingoList[3] = [
+    { name: "Kill a Boar", types: ["CREATURE"]},
+    { name: "Kill a Goat", types: ["CREATURE"]},
+    { name: "Kill a Wolf or Coyote", types: ["CREATURE"]},
+  ];
+  bingoList[4] = [
+    { name: "Kill a Buck or Doe", types: ["CREATURE"]},
+    { name: "Kill a Buffalo", types: ["CREATURE"]},
+    { name: "Kill a Moose", types: ["CREATURE"]},
+    { name: "Kill a Great-Horned Rhinoceros", types: ["CREATURE"]},
+  ];
+  bingoList[5] = [
+    { name: "Kill a Chuchu", types: ["CHUCHU", "MONSTER"]},
+    { name: "Kill a Keese", types: ["KEESE", "MONSTER"]},
+    { name: "Kill an Octorok", types: ["OCTOROK", "MONSTER"]},
+    { name: "Kill a Peblit", types: ["TALUS", "MONSTER"]},
+    { name: "Kill an entire group of Peblit", types: ["TALUS", "MONSTER", "MASSACRE"]},
+    { name: "Kill an group of 3+ Chuchu", types: ["CHUCHU", "MONSTER", "MASSACRE"]},
+  ];
+  bingoList[6] = [
+    { name: "Kill any Stal monster", types: ["STAL", "MONSTER"]},
+    { name: "Kill a group of 5+ Keese", types: ["KEESE", "MONSTER", "MASSACRE"]},
+  ];
+  bingoList[7] = [
+    { name: "Kill a Bokoblin", types: ["BOKOBLIN", "MONSTER"]},
+    { name: "Kill a Moblin", types: ["MOBLIN", "MONSTER"]},
+    { name: "Kill a Lizalfos", types: ["LIZALFOS", "MONSTER"]},
+    { name: "Kill a Yiga Footsoldier", types: ["YIGA", "MONSTER"]},
+  ];
+  bingoList[8] = [
+    { name: "Kill a Level 2 Monster [Blue]", types: ["MONSTER", "LEVEL"]},
+    { name: "Kill one of each basic Stal [LIZALFOS, BOKOBLIN, MOBLIN]", types: ["STAL", "MONSTER"]},
+  ];
+  bingoList[9] = [
+    { name: "Kill an entire camp of Bokoblin", types: ["BOKOBLIN", "MONSTER", "MASSACRE"]},
+  ];
+  bingoList[10] = [
+    { name: "Kill a Wizzrobe", types: ["WIZZROBE", "MONSTER"]},
+    { name: "Kill an entire group of 3+ Moblin", types: ["MOBLIN", "MONSTER", "MASSACRE"]},
+  ];
+  bingoList[11] = [
+    { name: "Kill a Bear", types: ["CREATURE"]},
+    { name: "Kill a Yiga Blademaster", types: ["YIGA", "MONSTER"]},
+  ];
+  bingoList[12] = [
+    { name: "Kill a Chuchu of each element [FIRE, ICE, ELECTRIC]", types: ["CHUCHU", "MONSTER", "ELEMENT"]},
+    { name: "Kill a Keese of each element [FIRE, ICE, ELECTRIC]", types: ["KEESE", "MONSTER", "ELEMENT"]},
+    { name: "Kill a Peblit of each element [IGNEO, FROST]", types: ["TALUS", "MONSTER"]},
+  ];
+  bingoList[13] = [
+    { name: "Kill a Guardian Scout", types: ["GUARDIAN", "MONSTER"]},
+    { name: "Kill a Level 3 Monster [Black]", types: ["MONSTER", "LEVEL"]},
+  ];
+  bingoList[14] = [
+    { name: "Kill an entire group of 3+ Lizalfos", types: ["LIZALFOS", "MONSTER", "MASSACRE"]},
+  ];
+  bingoList[15] = [
+    { name: "Kill a Wizzrobe of each element [FIRE, ICE, ELECTRIC]", types: ["WIZZROBE", "MONSTER", "ELEMENT"]},
+    { name: "Kill an Octorok of each type [TREASURE, WATER, SKY, SNOW, ROCK, FOREST]", types: ["OCTOROK", "MONSTER"]},
+  ];
+  bingoList[16] = [
+    { name: "Kill a Lizalfos of each element [FIRE, ICE, ELECTRIC]", types: ["LIZALFOS", "MONSTER", "ELEMENT"]},
+    { name: "Kill a Talus", types: ["TALUS", "MONSTER"]},
+    { name: "Kill a Hinox", types: ["HINOX", "MONSTER"]},
+    { name: "Kill a Stalnox", types: ["HINOX", "STAL", "MONSTER"]},
+  ];
+  bingoList[17] = [
+    { name: "Kill a Talus of each type [LUMINOUS, RARE]", types: ["TALUS", "MONSTER"]},
+    { name: "Kill 3 different Hinox", types: ["HINOX", "MONSTER", "GENOCIDE"]},
+  ];
+  bingoList[18] = [
+    { name: "Kill 3 different Talus", types: ["TALUS", "MONSTER", "GENOCIDE"]},
+    { name: "Kill 5 different Hinox", types: ["HINOX", "MONSTER", "GENOCIDE"]},
+    { name: "Kill a Decayed Guardian", types: ["GUARDIAN", "MONSTER"]},
+    { name: "Kill a Level 4 Monster [White/Silver]", types: ["MONSTER", "LEVEL"]},
+  ];
+  bingoList[19] = [
+    { name: "Kill a Talus of each element [IGNEO, FROST]", types: ["TALUS", "MONSTER"]},
+    { name: "Kill 3 different Stalnox", types: ["HINOX", "STAL", "MONSTER", "GENOCIDE"]},
+  ];
+  bingoList[20] = [
+    { name: "Kill 5 different Talus", types: ["TALUS", "MONSTER", "GENOCIDE"]},
+  ];
+  bingoList[21] = [
+    { name: "Kill 10 different Hinox", types: ["HINOX", "STAL", "MONSTER", "GENOCIDE"]},
+    { name: "Kill a Guardian Skywatcher", types: ["GUARDIAN", "MONSTER"]},
+    { name: "Kill a Guardian Turret", types: ["GUARDIAN", "MONSTER"]},
+  ];
+  bingoList[22] = [
+    { name: "Kill a Molduga", types: ["MOLDUGA", "MONSTER"]},
+    { name: "Kill a Guardian Stalker", types: ["GUARDIAN", "MONSTER"]},
+  ];
+  bingoList[23] = [
+    { name: "Kill a Lynel", types: ["LYNEL", "MONSTER"]},
+  ];
+  bingoList[24] = [
+    { name: "Kill 5 different Stalnox", types: ["HINOX", "STAL", "MONSTER", "GENOCIDE"]},
+    { name: "Kill 10 different Talus", types: ["TALUS", "MONSTER", "GENOCIDE"]},
+    { name: "Kill the Coliseum Lynel", types: ["LYNEL", "MONSTER"]},
+  ];
+  bingoList[25] = [
+    { name: "Kill 3 different Lynel without using Ancient arrows", types: ["LYNEL", "MONSTER", "GENOCIDE"]},
+    { name: "Kill all 4 Molduga", types: ["MOLDUGA", "MONSTER", "GENOCIDE"]},
+    { name: "Destroy all 26 Guardian Stalkers in Hyrule Field", types: ["GUARDIAN", "MONSTER", "GENOCIDE"]},
+  ];
